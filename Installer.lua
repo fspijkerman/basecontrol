a4c25f54095ef65472879f8558dcc7e627537e42
Frank Spijkerman <fspijkerman@gmail.com>
2018-05-19 22:07:05 +0200

    license language fix

--- Installer.lua
+++ Installer.lua
@@ -204,7 +204,7 @@ end
 
 stages[2] = function()
 	addButtonsToStage()
-	stageContainer:addChild(GUI.textBox(1, 1, 90, 20, 0xFFFFFF, 0x444444, string.wrap(web.request(URLs.license .. MineOSCore.properties.language .. ".lang"), 88), 1, 1, 1))
+	stageContainer:addChild(GUI.textBox(1, 1, 90, 20, 0xFFFFFF, 0x444444, string.wrap(web.request(URLs.license), 88), 1, 1, 1))
 
 	stageContainer.nextStageButton.disabled = true
 	local switch = addSwitchToStage(41, 22, 0x666666, localization.terms, false)
